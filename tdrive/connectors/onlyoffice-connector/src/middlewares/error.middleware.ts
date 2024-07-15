606a1fd14a15a4dc444d8a02e84ec30d1fa7e812
Anton SHEPILOV <ashepilov@linagora.com>
2024-07-15 16:00:49 +0200

    💄Added user friendly error page instead of JSON response

--- tdrive/connectors/onlyoffice-connector/src/middlewares/error.middleware.ts
+++ tdrive/connectors/onlyoffice-connector/src/middlewares/error.middleware.ts
@@ -1,14 +1,20 @@
 import logger from '@/lib/logger';
 import { NextFunction, Request, Response } from 'express';
+import * as Utils from '@/utils';
+import { SERVER_ORIGIN, SERVER_PREFIX } from '@config';
 
 export default (error: Error & { status?: number }, req: Request, res: Response, next: NextFunction): void => {
   try {
     const status: number = error.status || 500;
-    const message: string = error.message || 'something went wrong';
+    const message: string = error.message || 'Something went wrong';
 
     logger.error(`[${req.method}] ${req.path} >> StatusCode:: ${status}, Message:: ${message}`, error.stack);
 
-    res.status(status).json({ message });
+    res.status(status);
+    res.render('error', {
+      server: Utils.joinURL([SERVER_ORIGIN, SERVER_PREFIX]),
+      errorMessage: message,
+    });
   } catch (error) {
     next(error);
   }
