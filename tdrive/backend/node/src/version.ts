a921942f7d83779e6f36b2d70b824c00b96c94c9
shepilov <anton.shepilov@gmail.com>
2025-04-15 12:30:15 +0200

    ⬆️New rc with the latest api updates

--- tdrive/backend/node/src/version.ts
+++ tdrive/backend/node/src/version.ts
@@ -1,7 +1,7 @@
 export default {
-  current: /* @VERSION_DETAIL */ "1.0.7-rc7",
+  current: /* @VERSION_DETAIL */ "1.0.7-rc8",
   minimal: {
-    web: /* @MIN_VERSION_WEB */ "1.0.7-rc7",
-    mobile: /* @MIN_VERSION_MOBILE */ "1.0.7-rc7",
+    web: /* @MIN_VERSION_WEB */ "1.0.7-rc8",
+    mobile: /* @MIN_VERSION_MOBILE */ "1.0.7-rc8",
   },
 };
--- tdrive/frontend/src/app/environment/version.ts
+++ tdrive/frontend/src/app/environment/version.ts
@@ -1,5 +1,5 @@
 export default {
-  version: /* @VERSION */ '1.0.7-rc7',
-  version_detail: /* @VERSION_DETAIL */ '1.0.7-rc7',
+  version: /* @VERSION */ '1.0.7-rc8',
+  version_detail: /* @VERSION_DETAIL */ '1.0.7-rc8',
   version_name: /* @VERSION_NAME */ 'Ghost-Dog',
 };
