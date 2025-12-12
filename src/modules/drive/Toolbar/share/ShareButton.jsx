3cec357f12bddf60769a764b6d6c75e2fdaf3c1a
doubleface <christophe@cozycloud.cc>
2025-12-12 10:58:04 +0100

    fix: Change shared button size on mobile
    + padding for better actions distribution

--- src/modules/drive/Toolbar/share/ShareButton.jsx
+++ src/modules/drive/Toolbar/share/ShareButton.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
 
 import { ShareButton } from 'cozy-sharing'
+import useBreakpoints from 'cozy-ui/transpiled/react/providers/Breakpoints'
 
 import { useDisplayedFolder } from '@/hooks'
 import { getPathToShareDisplayedFolder } from '@/modules/drive/Toolbar/share/helpers'
@@ -10,6 +11,7 @@ const ShareButtonWithProps = ({ isDisabled, className, useShortLabel }) => {
   const { displayedFolder } = useDisplayedFolder()
   const navigate = useNavigate()
   const { pathname } = useLocation()
+  const { isMobile } = useBreakpoints()
 
   const share = () => {
     navigate(getPathToShareDisplayedFolder(pathname))
@@ -24,6 +26,7 @@ const ShareButtonWithProps = ({ isDisabled, className, useShortLabel }) => {
       useShortLabel={useShortLabel}
       className={className}
       onClick={() => share(displayedFolder)}
+      size={isMobile ? 'small' : 'medium'}
     />
   )
 }
