75c6bf4568703ce84539ccc6cc038732e8cbc564
Théo Poizat <hello@zatteo.com>
2026-04-08 20:29:34 +0200

    fix: Try to create a public download link

--- src/modules/services/components/Picker.jsx
+++ src/modules/services/components/Picker.jsx
@@ -15,7 +15,27 @@ const Picker = ({ service }) => {
     //   makeSharingLink(client, [fileId])
     // ])
 
+    // "http://drive.alice.cozy.localhost:8080/public?sharecode=efiOchmwc9oU#/" }
+    // POST "http://alice.cozy.localhost:8080/files/downloads?Id=01c6d8cb446b41c3a71fe846bc0dcd46&Filename=La%20Suite%20documentation%20technique.pdf"
+    // avec en Bearer le sharecode
+    // GET "http://alice.cozy.localhost:8080/files/downloads/bd5db658030a6bd3/La%20Suite%20documentation%20technique.pdf?Dl=1"
+
     const sharingLink = await makeSharingLink(client, [fileId])
+
+    // const url = new URL(sharingLink)
+    // const searchParams = new URLSearchParams(url.search)
+    // const sharecode = searchParams.get('sharecode')
+
+    // const publicClient = new CozyClient({
+    //   cozyUrl: client.getStackClient().uri,
+    //   token: sharecode,
+    //   useCustomStore: true
+    // })
+
+    // const downloadLink = await publicClient
+    //   .collection('io.cozy.files')
+    //   .getDownloadLinkById(fileId, 'test.pdf')
+
     service.terminate({ id: fileId, sharingLink })
   }
 
