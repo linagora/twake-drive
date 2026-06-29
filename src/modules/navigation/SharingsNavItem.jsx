457a9de174983f6b1933ce613cacaa76058298e9
doubleface <mr.thiriot@gmail.com>
2026-06-29 16:45:23 +0200

    fix(sharings): make sidebar counter consistent with sharings view
    Replace the independent CouchDB query for new sharing shortcuts in
    the sidebar badge with the same query used by the sharings body
    (buildSharingsQuery via useSharingContext). This ensures the counter
    only counts items that are actually present in the sharings view.

--- src/modules/navigation/SharingsNavItem.jsx
+++ src/modules/navigation/SharingsNavItem.jsx
@@ -1,18 +1,30 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 
 import { useQuery } from 'cozy-client'
+import { isSharingShortcutNew } from 'cozy-client/dist/models/file'
+import { useSharingContext } from 'cozy-sharing'
 import Icon from 'cozy-ui/transpiled/react/Icon'
 import ShareIcon from 'cozy-ui/transpiled/react/Icons/ShareExternal'
 
 import { NavItem } from '@/modules/navigation/NavItem'
-import { buildNewSharingShortcutQuery } from '@/queries'
+import { buildSharingsQuery } from '@/queries'
 
 const SharingsNavItem = ({ clickState }) => {
-  const newSharingShortcutQuery = buildNewSharingShortcutQuery()
-  const newSharingShortcutResult = useQuery(
-    newSharingShortcutQuery.definition,
-    newSharingShortcutQuery.options
+  const { byDocId, allLoaded } = useSharingContext()
+  const sharedDocuments = useMemo(
+    () => [...new Set(Object.keys(byDocId ?? {}))],
+    [byDocId]
   )
+
+  const sharingQuery = useMemo(
+    () =>
+      buildSharingsQuery({
+        ids: sharedDocuments,
+        enabled: allLoaded && sharedDocuments.length > 0
+      }),
+    [sharedDocuments, allLoaded]
+  )
+  const sharingResult = useQuery(sharingQuery.definition, sharingQuery.options)
 
   return (
     <NavItem
@@ -21,7 +33,9 @@ const SharingsNavItem = ({ clickState }) => {
       label="sharings"
       rx={/\/sharings(\/.*)?|\/shareddrive(\/.*)?/}
       clickState={clickState}
-      badgeContent={newSharingShortcutResult.data?.length}
+      badgeContent={
+        sharingResult.data?.filter(isSharingShortcutNew).length ?? 0
+      }
     />
   )
 }
