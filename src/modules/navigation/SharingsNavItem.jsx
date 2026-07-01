9d20e0b4e1b2dce4ddf7819badd86abca456f428
Théo Poizat <hello@zatteo.com>
2026-07-01 11:35:56 +0200

    fix: Highlight correctly shared folder and shared drive items
    New rules:
    - for each shared drive, each item need to be displayed if the shared
    drive is opened so checking if the pathname correspond to
    /shareddrive/{shared drive id} is good
    - for "Sharings", it is a tab and we want it to be displayed when we
    are on "Sharings" tab but also when we browse in shared folders
    inside this tab. So we introduce an isActive method to highlight in
    /shareddrive/ only if the id correspond to a shared folder and not a
    shared drive

--- src/modules/navigation/SharingsNavItem.jsx
+++ src/modules/navigation/SharingsNavItem.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react'
+import React, { useCallback, useMemo } from 'react'
 
 import { useQuery } from 'cozy-client'
 import { isSharingShortcutNew } from 'cozy-client/dist/models/file'
@@ -7,13 +7,25 @@ import Icon from 'cozy-ui/transpiled/react/Icon'
 import ShareIcon from 'cozy-ui/transpiled/react/Icons/ShareExternal'
 
 import { NavItem } from '@/modules/navigation/NavItem'
+import { useSharedDrives } from '@/modules/shareddrives/hooks/useSharedDrives'
 import { buildSharingsQuery } from '@/queries'
 
 const SharingsNavItem = ({ clickState }) => {
   const { byDocId, allLoaded } = useSharingContext()
+  const { sharedDrives } = useSharedDrives()
   const sharedDocuments = useMemo(
     () => [...new Set(Object.keys(byDocId ?? {}))],
     [byDocId]
+  )
+
+  const orgDriveIds = useMemo(
+    () =>
+      new Set(
+        (sharedDrives || [])
+          .filter(drive => drive.org_drive)
+          .map(drive => drive._id)
+      ),
+    [sharedDrives]
   )
 
   const sharingQuery = useMemo(
@@ -26,12 +38,26 @@ const SharingsNavItem = ({ clickState }) => {
   )
   const sharingResult = useQuery(sharingQuery.definition, sharingQuery.options)
 
+  const isActive = useCallback(
+    pathname => {
+      // For sharing tab
+      if (pathname === '/sharings') return true
+      // For shared folder but not shared drive
+      if (pathname.startsWith('/shareddrive/')) {
+        const driveId = pathname.split('/')[2]
+        return !orgDriveIds.has(driveId)
+      }
+      return false
+    },
+    [orgDriveIds]
+  )
+
   return (
     <NavItem
       to="/sharings"
       icon={<Icon icon={ShareIcon} />}
       label="sharings"
-      rx={/\/sharings(\/.*)?|\/shareddrive(\/.*)?/}
+      isActive={isActive}
       clickState={clickState}
       badgeContent={
         sharingResult.data?.filter(isSharingShortcutNew).length ?? 0
