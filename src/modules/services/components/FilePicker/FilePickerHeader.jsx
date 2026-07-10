90c19b6c124d86419e542371d566bf9408cd725d
doubleface <mr.thiriot@gmail.com>
2026-07-10 18:11:50 +0200

    fix(file-picker): prevent duplicate close buttons

--- src/modules/services/components/FilePicker/FilePickerHeader.jsx
+++ src/modules/services/components/FilePicker/FilePickerHeader.jsx
@@ -1,34 +1,14 @@
-import { CrossMedium, Icon } from '@linagora/twake-icons'
-import PropTypes from 'prop-types'
-import React, { memo } from 'react'
+import React from 'react'
 
 import AppTitle from 'cozy-ui/transpiled/react/AppTitle'
-import IconButton from 'cozy-ui/transpiled/react/IconButton'
-import useBreakpoints from 'cozy-ui/transpiled/react/providers/Breakpoints'
 
 import Drive from '@/components/Icons/Drive'
 import DriveText from '@/components/Icons/DriveText'
 
-const FilePickerHeader = ({ onClose }) => {
-  const { isMobile } = useBreakpoints()
+const FilePickerHeader = () => (
+  <div className="u-flex u-flex-items-center" data-testid="file-picker-header">
+    <AppTitle appIcon={Drive} appTextIcon={DriveText} />
+  </div>
+)
 
-  return (
-    <div
-      className="u-flex u-flex-items-center"
-      data-testid="file-picker-header"
-    >
-      <AppTitle appIcon={Drive} appTextIcon={DriveText} />
-      {isMobile && (
-        <IconButton onClick={onClose} className="u-ml-auto" size="medium">
-          <Icon icon={CrossMedium} />
-        </IconButton>
-      )}
-    </div>
-  )
-}
-
-FilePickerHeader.propTypes = {
-  onClose: PropTypes.func.isRequired
-}
-
-export default memo(FilePickerHeader)
+export default FilePickerHeader
