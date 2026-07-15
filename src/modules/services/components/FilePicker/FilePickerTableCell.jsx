dd6d192992f64077406343c170e589ed2aa8d710
doubleface <mr.thiriot@gmail.com>
2026-07-15 17:53:13 +0200

    refactor(file-picker): reuse drive table cells

--- src/modules/services/components/FilePicker/FilePickerTableCell.jsx
+++ src/modules/services/components/FilePicker/FilePickerTableCell.jsx
@@ -1,16 +1,13 @@
-import { filesize } from 'filesize'
 import PropTypes from 'prop-types'
 import React from 'react'
 
-import { isDirectory } from 'cozy-client/dist/models/file'
 import Filename from 'cozy-ui/transpiled/react/Filename'
 import { useI18n } from 'twake-i18n'
 
 import { getFileNameAndExtension } from '@/modules/filelist/helpers'
 import FileThumbnail from '@/modules/filelist/icons/FileThumbnail'
-import { useFormattedUpdatedAt } from '@/modules/filelist/useFormattedUpdatedAt'
-import LastUpdate from '@/modules/filelist/virtualized/cells/LastUpdate'
-import Size from '@/modules/filelist/virtualized/cells/Size'
+import SizeCell from '@/modules/filelist/virtualized/cells/columns/SizeCell'
+import UpdatedAtCell from '@/modules/filelist/virtualized/cells/columns/UpdatedAtCell'
 
 const FilePickerNameCell = ({ row }) => {
   const { t } = useI18n()
@@ -39,35 +36,16 @@ FilePickerNameCell.propTypes = {
   row: PropTypes.object.isRequired
 }
 
-const FilePickerLastUpdateCell = ({ row }) => {
-  const updatedAt = row.updated_at || row.created_at
-  const formattedUpdatedAt = useFormattedUpdatedAt(updatedAt)
-
-  if (!formattedUpdatedAt) return '—'
-
-  return <LastUpdate date={updatedAt} formatted={formattedUpdatedAt} />
-}
-
-FilePickerLastUpdateCell.propTypes = {
-  row: PropTypes.object.isRequired
-}
-
-const FilePickerSizeCell = ({ row }) => {
-  if (isDirectory(row) || !row.size) return '—'
-
-  return <Size filesize={filesize(row.size, { base: 10 })} />
-}
-
-FilePickerSizeCell.propTypes = {
-  row: PropTypes.object.isRequired
-}
-
 export const FilePickerTableCell = ({ column, row }) => {
   if (!column || !row) return null
 
   if (column.id === 'name') return <FilePickerNameCell row={row} />
-  if (column.id === 'updated_at') return <FilePickerLastUpdateCell row={row} />
-  if (column.id === 'size') return <FilePickerSizeCell row={row} />
+  if (column.id === 'updated_at') {
+    return <UpdatedAtCell row={row} cell={row.updated_at || row.created_at} />
+  }
+  if (column.id === 'size') {
+    return <SizeCell row={row} cell={row.size} />
+  }
 
   return null
 }
