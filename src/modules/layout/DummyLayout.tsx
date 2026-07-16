7a9548e1e72c20e0875b6243cb1274181d99e5d6
doubleface <mr.thiriot@gmail.com>
2026-07-16 11:25:58 +0200

    refactor(icons): remove sprite usage
    Stop injecting the hidden SVG sprite in application layouts and replace its two string-based icon references with individual components. This avoids parsing and keeping the full sprite in the DOM while allowing the icons to be bundled independently.

--- src/modules/layout/DummyLayout.tsx
+++ src/modules/layout/DummyLayout.tsx
@@ -1,15 +1,9 @@
-import { Sprite } from '@linagora/twake-icons'
 import React from 'react'
 
 import { Layout } from 'cozy-ui/transpiled/react/Layout'
 
 const DummyLayout: React.FC = ({ children }) => {
-  return (
-    <Layout monoColumn={true}>
-      {children}
-      <Sprite />
-    </Layout>
-  )
+  return <Layout monoColumn={true}>{children}</Layout>
 }
 
 export { DummyLayout }
